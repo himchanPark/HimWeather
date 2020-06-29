ebb1392c3de0ad2722e196ecc654ea3644beed9b
himchanPark <ting1099@gmail.com>
2020-06-29 17:01:22 +0900

    테이블뷰 데이터소스

--- HimWeather/ViewController.swift
+++ HimWeather/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var listTableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,3 +22,34 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 0
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: SummaryTableViewCell.identifier, for: indexPath) as! SummaryTableViewCell
+            
+            return cell
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: ForecastTableViewCell.identifier, for: indexPath)
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+}
